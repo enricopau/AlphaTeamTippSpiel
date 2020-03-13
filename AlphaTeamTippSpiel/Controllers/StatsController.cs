fd2eba72e44777e91b2c3558deea8689155cb300
Enrico Pau <32924566+enricopau@users.noreply.github.com>
2020-03-13 14:44:41 +0100

    Added Stats View & Controller

---
+++ AlphaTeamTippSpiel/Controllers/StatsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace AlphaTeamTippSpiel.Controllers
+{
+    public class StatsController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name)
+        {
+            this.ViewData["Nachricht"] = "Hallo " + name + ",\nWillkommen auf deiner persönlichen Statistiken-Seite!";
+            
+            return this.View();
+        }
+    }
+}
